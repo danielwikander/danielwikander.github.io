3d05846e95c39c078947c837344d04a35adceace
Daniel Wikander <daniel.wikander@gmail.com>
2019-08-16 20:47:43 +0200

    added new site

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function jumpContactBox() {
+    document.getElementById("contactheader").style.animation = "jump 1s ease 1s 1";
+}
+
+function fadeArrow() {
+    document.getElementById("arrow").style.animation = "fadeout 1s ease 0.5s forwards";
+}
+
+
